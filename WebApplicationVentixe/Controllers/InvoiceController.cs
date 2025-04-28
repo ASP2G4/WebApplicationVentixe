47e341e1067db203a08587db9a49ec7d5265a4fb
Tim Nyman <timnyman@live.com>
2025-04-28 14:35:38 +0200

    Added invoice controller, view and then made sure the sidebar navigates to the correct place + loads the information.

---
+++ WebApplicationVentixe/Controllers/InvoiceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationVentixe.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Invoices";
+            return View();
+        }
+    }
+}
