b60b92b86221b31041a5296cbf8b85d85d7d3023
Marcus BV <marcus.backstromviol@gmail.com>
2025-05-07 15:37:07 +0200

    Example bookings

---
+++ WebApplicationVentixe/Controllers/BookingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationVentixe.Controllers
+{
+    public class BookingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Bookings";
+            return View();
+        }
+    }
+}
