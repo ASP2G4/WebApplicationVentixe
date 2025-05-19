d122544cd6c3c30dbf1c1aab3bc3cfb3c674b2b5
Tim Nyman <timnyman@live.com>
2025-05-19 13:23:47 +0200

    Fixed my protos file to match backend. Added search button that searches on Id, can add more options if needed / wants. Made a Model for my Invoice (InvoiceViewModel).

---
+++ WebApplicationVentixe/Models/Invoice/InvoiceViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationVentixe.Models.Invoice;
+
+public class InvoiceViewModel
+{
+    public int Id { get; set; }
+    public string InvoiceNumber => $"INV100{Id}";
+
+    public DateTime StartDate { get; set; }
+
+    public decimal TicketPrice { get; set; }
+
+    public string? StatusName { get; set; }
+}
