ced01aba84f9cf890f0677944fa785221b89f3a7
Winternet <davidcarlstedt@hotmail.com>
2025-05-27 13:43:30 +0200

    Events

---
+++ WebApplicationVentixe/Models/Events/UpdateEventFormData.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationVentixe.Models.Events;
+
+public class UpdateEventFormData
+{
+    public string Id { get; set; } = null!;
+    public string EventName { get; set; } = null!;
+    public string EventDescription { get; set; } = null!;
+    public string EventLocation { get; set; } = null!;
+    public DateTime EventDate { get; set; }
+}
